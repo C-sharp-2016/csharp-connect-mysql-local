92b02febaea927dae027e8d5c33fcc6715e91f6e
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-06-28 21:50:53 +0800

    connect to local database and use simple query of the entire table

--- mysql_connect_local/mysql_connect_local/Form1.cs
+++ mysql_connect_local/mysql_connect_local/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace mysql_connect_local
 {
@@ -15,6 +16,31 @@ namespace mysql_connect_local
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection mcon = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+
+                MySqlDataAdapter mda = new MySqlDataAdapter("Select * from  csharp_testing1.users", mcon);
+
+                mcon.Open();
+
+                DataSet ds = new DataSet();
+
+                mda.Fill(ds, "users");
+
+                dataGridView1.DataSource = ds.Tables["users"];
+
+                mcon.Close(); 
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
